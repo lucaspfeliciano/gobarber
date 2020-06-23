ba96e1b71ff566d297124904fe285c4b5115ad74
Lucas  Feliciano <lucas.feliciano@bry.com.br>
2020-06-23 12:01:52 -0300

    Initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+
+import express, { response } from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server start on port 3333!');
+});
