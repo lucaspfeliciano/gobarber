183ba0195a751b2679821bb4d875abb374847d9c
Lucas  Feliciano <lucas.feliciano@bry.com.br>
2020-07-03 12:06:21 -0300

    Adiciona toasts e tooltips

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
 import GlobalStyle from './styles/global';
+
+import AppProvider from './hooks/index';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <AppProvider>
+      <SignIn />
+    </AppProvider>
+
     <GlobalStyle />
   </>
 );
