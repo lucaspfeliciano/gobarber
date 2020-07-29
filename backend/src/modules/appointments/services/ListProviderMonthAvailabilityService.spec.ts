4f50aff66d8c1a41b8497143eaf1d75cdf14017e
Lucas  Feliciano <lucas.feliciano@bry.com.br>
2020-07-29 11:38:56 -0300

    Adiciona funcionalidade pra ver dias e horarios disponiveis de um prestador

---
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,44 @@
+// import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the month availability from provider', async () => {
+    for (let i = 8; i < 18; i++) {
+      await fakeAppointmentsRepository.create({
+        provider_id: 'user',
+        date: new Date(2020, 8, 20, i, 0, 0),
+      });
+    }
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 9, 21, 8, 0, 0),
+    });
+
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 9,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
+      ]),
+    );
+  });
+});
