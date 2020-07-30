9c6d4bb49bf818411c33c8a2ed291dec72bfd11a
Lucas  Feliciano <lucas.feliciano@bry.com.br>
2020-07-30 11:45:12 -0300

    Adiciona listagens de agendamentos de prestador logado

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,38 @@
+// import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 10, 20, 9, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user2',
+      date: new Date(2020, 10, 20, 8, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      day: 20,
+      year: 2020,
+      month: 11,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
